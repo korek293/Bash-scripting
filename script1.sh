f9061ddd6002bef518fd428d3634daf4a77724d9
Anton ubuntu <nuserant@gmail.com>
2024-08-06 16:32:09 +0200

    some changes

--- script1.sh
+++ script1.sh
@@ -2,18 +2,21 @@
 
 release_file=/etc/os-release
 
-if [ -d /etc/pacman.d ]
+if grep -q "Arch" $release_file
 then
+	#The host is based on Arch
 	sudo pacman -Syu
 fi
 
-if [ -d /etc/apt ]
+if grep -q "Ubuntu" $release_file || grep -q "Debian" $release_file
 then
+	#The host is based on Ubuntu or Debian
 	sudo apt update
 	sudo apt dist-upgrade
 fi
 
 if [ -d /etc/yum ]
 then
+	#The host is based on Red Hat
 	sudo yum update
 fi
