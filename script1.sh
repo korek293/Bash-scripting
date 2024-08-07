92da7b1a23ea62bfece2360a69f59b3bcd4b2a48
Anton ubuntu <nuserant@gmail.com>
2024-08-07 14:44:35 +0200

    update script

--- script1.sh
+++ script1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 release_file=/etc/os-release
+logfile=/var/log/updater.log
+errorlog=/var/log/updater_errors.log
 
 if grep -q "Arch" $release_file
 then
@@ -11,12 +13,24 @@ fi
 if grep -q "Ubuntu" $release_file || grep -q "Debian" $release_file
 then
 	#The host is based on Ubuntu or Debian
-	sudo apt update
-	sudo apt dist-upgrade
+	sudo apt update 1>>$logfile 2>>$errorlog
+	if [ $? -ne 0 ]
+	then
+		echo "An error occured, please check the $errorlog file."
+	fi
+	sudo apt dist-upgrade 1>>$logfile 2>>$errorlog
+	if [ $? -ne 0 ]
+	then
+		echo "An error occured, please check the $errorlog file."
+	fi
 fi
 
 if [ -d /etc/yum ]
 then
 	#The host is based on Red Hat
-	sudo yum update
+	sudo yum update 1>>$logfile 2>>$errorlog
+	if [ $? -ne 0 ]
+	then
+		echo "An error occured, please check the $errorlog file"
+	fi
 fi
